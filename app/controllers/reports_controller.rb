9c057be7ce2cbc97079e376a62e3183aab1b54ab
Ru-T <rwajnberg@gmail.com>
2015-10-21 09:51:43 -0400

    Simplify all_data controller action.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,28 +3,23 @@ class ReportsController < ApplicationController
 
   def all_data
     @start_time = Time.now
-
-    @sequences = []
-    @genes = []
-    @hits = []
     @assembly = Assembly.find_by_name(params[:name])
-    @assembly.sequences.each do |s|
-      @sequences << s
-      s.genes.each do |g|
-        @genes << g
-        g.hits.each do |h|
-          @hits << h
-        end
-      end
-    end
-    @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
-
     @memory_used = memory_in_mb
+
+    sequences = Sequence.where(assembly_id: @assembly.id)
+    sequence_ids = sequences.map {|s| s.id}
+
+    genes = Gene.where(sequence_id: sequence.ids)
+    gene_ids = genes.map {|g| g.id}
+
+    @hits = Hit.where(subject_id: gene_ids).order(percent_similarity: :desc)
   end
 
   def search
     if params[:search]
-      @hits = Hit.where("match_gene_name LIKE ?", "%#{params[:search]}")
+      @hits = Hit.joins(gene:[:assembly]).
+      where("match_gene_name LIKE ? or assembly.name LIKE ?",
+      "%#{params[:search]}", "%#{params[:search]}")
     end
   end
 
